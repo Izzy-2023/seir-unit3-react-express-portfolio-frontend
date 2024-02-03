ef43d262e5908b7814339f4abfb2f229c2307108
izzy-2023 <izzyzinxhirija@gmail.com>
2024-02-03 14:42:34 -0500

    upated url in loaders file

--- src/loaders.js
+++ src/loaders.js
@@ -1,6 +1,6 @@
 // BASE URL OF OUR DEPLOYED API
 // BASE URL OF OUR DEPLOYED API
-const URL = "https://portfolio-app-lb0f.onrender.com/"
+const URL = "https://portfolio-app-lb0f.onrender.com"
 
 export const projectsLoader = async () => {
     const response = await fetch(URL + "/projects")
